8a85f8b9fa3d45f7cd6cfd12cea77faa7c5c5759
SektorDV <roman@sawblade.pl>
2019-06-02 23:24:03 +0200

    background, elliptic controls, controls slide anim

--- src/Components/Controls/Controls.jsx
+++ src/Components/Controls/Controls.jsx
@@ -12,38 +12,45 @@ const Controls = (props) => {
         <h1>MTG Card Search</h1>
 
         <div className="controls__controls">
-            <div className="controls__controls__searchInput">
-              <input onChange={props.inputHandler} value={props.searchInput} type="text" placeholder="Enter card name..." />
-            </div>
-            <div className="controls__filters">
+          <div className="controls__controls__searchInput">
+            <input onChange={props.inputHandler} value={props.searchInput} type="text" placeholder="Enter card name..." />
+          </div>
+          <div className="controls__filters">
 
-        <label>
-        <input onChange={(e) => props.filterHandler({blueFilter: e.target.checked})} type="checkbox"/>
-        <span>Blue</span>
-        </label>
-        <label>
-        <input onChange={(e) => props.filterHandler({redFilter: e.target.checked})}type="checkbox"/>
-        <span>Red</span>
-        </label>
-        <label>
-        <input onChange={(e) => props.filterHandler({whiteFilter: e.target.checked})} type="checkbox"/>
-        <span>White</span>
-        </label>
-        <label>
-        <input onChange={(e) => props.filterHandler({blackFilter: e.target.checked})} type="checkbox"/>
-        <span>Black</span>
-        </label>
-        <label>
-        <input onChange={(e) => props.filterHandler({greenFilter: e.target.checked})} type="checkbox"/>
-        <span>Green</span>
-        </label>
-        
-        </div>
+            <label>
+              <input onChange={(e) => props.filterHandler({ blueFilter: e.target.checked })} type="checkbox" />
+              <span>Blue</span>
+            </label>
+            <label>
+              <input onChange={(e) => props.filterHandler({ redFilter: e.target.checked })} type="checkbox" />
+              <span>Red</span>
+            </label>
+            <label>
+              <input onChange={(e) => props.filterHandler({ whiteFilter: e.target.checked })} type="checkbox" />
+              <span>White</span>
+            </label>
+            <label>
+              <input onChange={(e) => props.filterHandler({ blackFilter: e.target.checked })} type="checkbox" />
+              <span>Black</span>
+            </label>
+            <label>
+              <input onChange={(e) => props.filterHandler({ greenFilter: e.target.checked })} type="checkbox" />
+              <span>Green</span>
+            </label>
+
+          </div>
         </div>
       </div>
+      <div className="controls__hide__pip" onClick={() => {
+        if (document.querySelector('.controls__main').classList.contains("active")) {
+          document.querySelector('.controls__main').classList.remove('active')
+        } else {
+          document.querySelector('.controls__main').classList.add('active')
+        }
+      }}></div>
     </div>
   );
 }
-    
+
 
 export default Controls;
