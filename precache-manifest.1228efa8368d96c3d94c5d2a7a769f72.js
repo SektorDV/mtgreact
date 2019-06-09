8ebffb3b02d50e15ef2223b79d92ed1cb8da5231
Michał Romańczuk <roman@sawblade.pl>
2019-06-09 10:36:09 +0200

    Updates

---
+++ precache-manifest.1228efa8368d96c3d94c5d2a7a769f72.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93304e28a584b243c510187124644846",
+    "url": "/mtgreact/index.html"
+  },
+  {
+    "revision": "3d9880a473662ce95290",
+    "url": "/mtgreact/static/css/2.c4259aea.chunk.css"
+  },
+  {
+    "revision": "6d246058e23f901acf30",
+    "url": "/mtgreact/static/css/main.dab52bab.chunk.css"
+  },
+  {
+    "revision": "3d9880a473662ce95290",
+    "url": "/mtgreact/static/js/2.daf5689e.chunk.js"
+  },
+  {
+    "revision": "6d246058e23f901acf30",
+    "url": "/mtgreact/static/js/main.0409f664.chunk.js"
+  },
+  {
+    "revision": "985b93c5cedea991d500",
+    "url": "/mtgreact/static/js/runtime~main.1fa66d4a.js"
+  },
+  {
+    "revision": "22598a58cf11e6f4fb8187eb8df4cc31",
+    "url": "/mtgreact/static/media/adequate-extralight-webfont.22598a58.woff2"
+  },
+  {
+    "revision": "b9e88281c6dc9a2a4fabab77fece0544",
+    "url": "/mtgreact/static/media/adequate-extralight-webfont.b9e88281.woff"
+  },
+  {
+    "revision": "d10d35afab4a1ead27a6f9aed92d7de5",
+    "url": "/mtgreact/static/media/back.d10d35af.jpg"
+  },
+  {
+    "revision": "5cc73beabe15f1aa456e5a2d7225137a",
+    "url": "/mtgreact/static/media/magic.5cc73bea.jpg"
+  },
+  {
+    "revision": "2327a9adbf1d3b895208b4d71da06637",
+    "url": "/mtgreact/static/media/satam___-webfont.2327a9ad.woff"
+  },
+  {
+    "revision": "697c6b069a20fe99d67d968bfb3b5da0",
+    "url": "/mtgreact/static/media/satam___-webfont.697c6b06.woff2"
+  }
+]);
